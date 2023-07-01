64a3c65f24aea8f47f35904e250f099f7b9eb561
vladtf <vlad.tiftilov@yahoo.com>
2023-07-01 15:11:53 +0300

    create virtual env if missing

--- prepare.sh
+++ prepare.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# flag to indicate this script was sourced
+# Flag to indicate this script was sourced
 export PREPARE_SH_SOURCED=true
 
-# save current directory
-BIN_DIR=$(dirname $0)
+# Save current directory
+BIN_DIR=$(dirname "$0")
 
-# declare alias to connect to test container
+# Declare alias to connect to the test container
 alias go-test='docker exec -it my-ubuntu bash'
-
-# source python virtual environment
-source $BIN_DIR/utils/myenv/bin/activate
 
 # Color codes
 GREEN='\033[0;32m'
@@ -28,20 +25,83 @@ log_success() {
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
     local log_message="[SUCCESS] $1"
     echo -e "${GREEN}$timestamp - $log_message${NC}"
-    echo -e "$timestamp - $log_message" >> "$LOG_FILE"
+    echo -e "$timestamp - $log_message" >>"$LOG_FILE"
 }
 
 log_error() {
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    local log_message="[ERROR] $1"so
+    local log_message="[ERROR] $1"
 
     echo -e "${RED}$timestamp - $log_message${NC}"
-    echo -e "$timestamp - $log_message" >> "$LOG_FILE"
+    echo -e "$timestamp - $log_message" >>"$LOG_FILE"
 }
 
 log_warning() {
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
     local log_message="[WARNING] $1"
     echo -e "${YELLOW}$timestamp - $log_message${NC}"
-    echo -e "$timestamp - $log_message" >> "$LOG_FILE"
+    echo -e "$timestamp - $log_message" >>"$LOG_FILE"
 }
+
+log_info() {
+    local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    local log_message="[INFO] $1"
+    echo -e "$timestamp - $log_message"
+    echo -e "$timestamp - $log_message" >>"$LOG_FILE"
+}
+
+# Create virtual environment
+VENV_DIR="$BIN_DIR/.venv"
+REQUIREMENTS_FILE="$BIN_DIR/requirements.txt"
+
+# Check if .venv directory exists
+if [ ! -d "$VENV_DIR" ]; then
+    log_warning "Virtual environment not found. Creating virtual environment..."
+    python3 -m venv "$VENV_DIR"
+
+    if [ $? -ne 0 ]; then
+        log_error "Failed to create virtual environment"
+        exit 1
+    else
+        log_success "Virtual environment created"
+    fi
+
+    log_info "Activating virtual environment"
+    source "$VENV_DIR/bin/activate"
+    if [ $? -ne 0 ]; then
+        log_error "Failed to activate virtual environment"
+        exit 1
+    else
+        log_success "Virtual environment activated"
+    fi
+
+    # Install requirements
+    if [ -f "$REQUIREMENTS_FILE" ]; then
+        log_info "Installing requirements"
+        pip install -r "$REQUIREMENTS_FILE"
+        if [ $? -ne 0 ]; then
+            log_error "Failed to install requirements"
+            exit 1
+        else
+            log_success "Requirements installed"
+        fi
+    else
+        log_error "Requirements file '$REQUIREMENTS_FILE' not found."
+        log_error "Please make sure to create the requirements file and try again."
+        exit 1
+    fi
+else
+    log_info "Activating virtual environment"
+    source "$VENV_DIR/bin/activate"
+    log_success "Virtual environment activated"
+
+    log_info "Installing requirements"
+    pip install -r "$REQUIREMENTS_FILE"
+
+    if [ $? -ne 0 ]; then
+        log_error "Failed to install requirements"
+        exit 1
+    else
+        log_success "Requirements installed"
+    fi
+fi
--- quick-start.sh
+++ quick-start.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 # save current directory
-BIN_DIR=$(dirname $0)
+BIN_DIR=$(dirname "$0")
+
+# Script variables
+prepare_script="$BIN_DIR/prepare.sh"
+kafka_compose_file="$BIN_DIR/docker-compose.yaml"
+test_container_name="my-ubuntu"
+test_network_name="mynetwork"
+
+###############################################
+# Do not modify anything below this line
+###############################################
+
+# Paths
+prepare_script_path="$BIN_DIR/prepare.sh"
+kafka_compose_file_path="$BIN_DIR/docker-compose.yaml"
+test_container_path="$BIN_DIR/test/ubuntu"
+test_container_dockerfile_path="$test_container_path/Dockerfile"
 
 # check if prepare.sh was sourced
 if [ -z "$PREPARE_SH_SOURCED" ]; then
@@ -9,45 +25,42 @@ if [ -z "$PREPARE_SH_SOURCED" ]; then
     exit 1
 fi
 
-# Container name
-test_container="my-ubuntu"
-
 # Source prepare.sh
-source "$BIN_DIR/prepare.sh"
+source "$prepare_script_path"
 if [ $? -ne 0 ]; then
     echo "failed to source prepare.sh"
     exit 1
 fi
 
 # check if kafka is running
-docker inspect kafka > /dev/null 2>&1
+docker-compose -f "$kafka_compose_file_path" ps kafka > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     # start kafka
-    docker-compose -f "$BIN_DIR/docker-compose.yaml" up -d
+    docker-compose -f "$kafka_compose_file_path" up -d
 else
     log_success "kafka already running"
 fi
 
 # check if test network exists
-docker network inspect mynetwork > /dev/null 2>&1
+docker network inspect "$test_network_name" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     # create test network
-    docker network create mynetwork
+    docker network create "$test_network_name"
 else
-    log_success "mynetwork already exists"
+    log_success "$test_network_name already exists"
 fi
 
 # check if test container exists
-docker inspect "$test_container" > /dev/null 2>&1
+docker inspect "$test_container_name" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     # remove test container
-    docker rm -f "$test_container" > /dev/null 2>&1
+    docker rm -f "$test_container_name" > /dev/null 2>&1
 else
-    log_success "$test_container does not exist"
+    log_success "$test_container_name does not exist"
 fi
 
 # build test container
-build_logs=$(docker build -t my-ubuntu -f "$BIN_DIR/test/ubuntu/Dockerfile" "$BIN_DIR/test/ubuntu" 2>&1)
+build_logs=$(docker build -t "$test_container_name" -f "$test_container_dockerfile_path" "$test_container_path" 2>&1)
 if [ $? -ne 0 ]; then
     log_error "build failed"
     log_error "$build_logs"
@@ -57,7 +70,7 @@ else
 fi
 
 # start test container
-run_logs=$(docker run -d --network=mynetwork --name "$test_container" my-ubuntu 2>&1)
+run_logs=$(docker run -d --network="$test_network_name" --name "$test_container_name" "$test_container_name" 2>&1)
 if [ $? -ne 0 ]; then
     log_error "run failed"
     log_error "$run_logs"
@@ -67,10 +80,13 @@ else
 fi
 
 # check if test container is running
-docker inspect "$test_container" | grep '"Running": true' > /dev/null 2>&1
+docker inspect "$test_container_name" | grep '"Running": true' > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    log_error "$test_container is not running"
+    log_error "$test_container_name is not running"
     exit 1
 else
-    log_success "$test_container is running"
+    log_success "$test_container_name is running"
 fi
+
+# start monitoring script
+python "$BIN_DIR/monitor-docker-traffic.py"
