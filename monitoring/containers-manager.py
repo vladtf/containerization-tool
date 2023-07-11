e8d77a4993c6b6d9462783b6010e8628ea381d5a
vladtf <vlad.tiftilov@yahoo.com>
2023-07-11 13:41:48 +0300

    fix not closing threads

--- monitoring/containers-manager.py
+++ monitoring/containers-manager.py
@@ -1,3 +1,4 @@
+import signal
 import time
 import json
 import logging
@@ -14,6 +15,15 @@ from containers import docker_client
 # Configure the logger
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
 logger = logging.getLogger("containers-manager")
+
+# Flag to signal threads to stop
+stop_threads = False
+
+
+def signal_handler(sig, frame):
+    global stop_threads
+    stop_threads = True
+    logger.info("Interrupt signal received. Stopping threads...")
 
 
 def kafka_producer(message: str, topic: str, bootstrap_servers: str):
@@ -42,13 +52,13 @@ def kafka_consumer(topic: str, group_id: str, callback: Callable[[str], None], b
     consumer.subscribe([topic])
 
     try:
-        while True:
+        while not stop_threads:
             messages = consumer.consume(10, timeout=1.0)
             for message in messages:
                 if message is None:
                     continue
                 if message.error():
-                    if message.error().code() == KafkaError._PARTITION_EOF:
+                    if message.error().code() == KafkaError.PARTITION_EOF:
                         continue
                     else:
                         logger.error("Kafka error: %s", message.error().str())
@@ -66,7 +76,7 @@ def prepare_docker_callback(callback: Callable[..., None], *args, **kwargs) -> C
 
 
 def monitor_containers_on_network(kafka_url: str, network_name: str, monitoring_interval: int):
-    while True:
+    while not stop_threads:
         containers_data = docker_client.list_containers_on_network(
             network_name)
 
@@ -79,12 +89,17 @@ def monitor_containers_on_network(kafka_url: str, network_name: str, monitoring_
 
 
 def main():
+    global stop_threads
+
     config = config_loader.load_config(os.path.abspath(__file__))
     kafka_url = config.get('kafka', 'bootstrap_servers')
     network_name = config.get('docker', 'network_name')
     base_image_path = config.get('docker', 'base_image_path')
 
     monitoring_interval = 5
+
+    # Set up signal handler for Ctrl+C
+    signal.signal(signal.SIGINT, signal_handler)
 
     create_callback = prepare_docker_callback(
         docker_client.create_docker_container, base_image_path=base_image_path, network_name=network_name)
@@ -103,22 +118,32 @@ def main():
     for thread in threads.values():
         thread.start()
 
-    while True:
+    try:
+        while not stop_threads:
+            for thread_name, thread in threads.items():
+                if not thread.is_alive():
+                    logger.error(
+                        "Thread '%s' is not alive. Restarting...", thread_name)
+                    if thread_name == "consumer_thread":
+                        threads[thread_name] = threading.Thread(target=kafka_consumer, args=(
+                            'create-container', 'my-group-create-container', create_callback, kafka_url))
+                    elif thread_name == "monitor_thread":
+                        threads[thread_name] = threading.Thread(target=monitor_containers_on_network, args=(
+                            kafka_url, network_name, monitoring_interval))
+                    elif thread_name == "delete_consumer_thread":
+                        threads[thread_name] = threading.Thread(target=kafka_consumer, args=(
+                            'delete-container', 'my-group-delete-container', delete_callback, kafka_url))
+                    threads[thread_name].start()
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        logger.info("Interrupted by user. Exiting...")
+
+    finally:
+        stop_threads = True
         for thread_name, thread in threads.items():
-            if not thread.is_alive():
-                logger.error(
-                    "Thread '%s' is not alive. Restarting...", thread_name)
-                if thread_name == "consumer_thread":
-                    threads[thread_name] = threading.Thread(target=kafka_consumer, args=(
-                        'create-container', 'my-group-create-container', create_callback, kafka_url))
-                elif thread_name == "monitor_thread":
-                    threads[thread_name] = threading.Thread(target=monitor_containers_on_network, args=(
-                        kafka_url, network_name, monitoring_interval))
-                elif thread_name == "delete_consumer_thread":
-                    threads[thread_name] = threading.Thread(target=kafka_consumer, args=(
-                        'delete-container', 'my-group-delete-container', delete_callback, kafka_url))
-                threads[thread_name].start()
-        time.sleep(1)
+            thread.join()
+            logger.info("Thread '%s' has stopped.", thread_name)
 
 
 if __name__ == '__main__':
