b735df0eb1c615e629bddbe1f499211c99bf9e69
vladtf <vlad.tiftilov@yahoo.com>
2023-07-18 18:22:27 +0300

    add thread pool implementation

--- monitoring/containers-manager.py
+++ monitoring/containers-manager.py
@@ -3,7 +3,6 @@ import logging
 import os
 import signal
 import sys
-import threading
 import time
 
 from confluent_kafka import Producer, Consumer
@@ -12,29 +11,16 @@ from configuration import config_loader
 from containers import docker_client
 from kafka.kafka_client import consume_kafka_message, create_kafka_producer, create_kafka_consumer, FeedbackMessage, \
     Level
+from threads.thread_pool import ThreadPool
 
 # Configure the logger
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
 logger = logging.getLogger(__name__)
 
-# Global variable to stop threads
-# TODO: to replace with an thread pool
-stop_threads = False
-
 
 # Stop application
-def stop_application(threads: dict, containers_data_producer: Producer, create_container_consumer: Consumer,
-                     delete_container_consumer: Consumer):
-    global stop_threads
-
-    logger.info("Stopping application...")
-
-    stop_threads = True
-
-    for thread_name, thread in threads.items():
-        logger.info("Stopping thread '%s'...", thread_name)
-        thread.join()
-
+def cleanup_task(containers_data_producer: Producer, create_container_consumer: Consumer,
+                 delete_container_consumer: Consumer):
     # Close Kafka producer and consumer
     containers_data_producer.flush()
 
@@ -44,122 +30,83 @@ def stop_application(threads: dict, containers_data_producer: Producer, create_c
 
 
 # Signal handler
-def stop_threads_handler(threads: dict, containers_data_producer: Producer, create_container_consumer: Consumer,
+def stop_threads_handler(thread_pool: ThreadPool,
+                         containers_data_producer: Producer,
+                         create_container_consumer: Consumer,
                          delete_container_consumer: Consumer):
     def signal_handler(sig, frame):
-        global stop_threads
-        stop_threads = True
         logger.info("Interrupt signal received. Stopping application...")
-        stop_application(threads, containers_data_producer, create_container_consumer, delete_container_consumer)
+        thread_pool.stop_threads()
+        cleanup_task(containers_data_producer, create_container_consumer, delete_container_consumer)
 
     return signal_handler
 
 
 def monitor_containers_task(producer: Producer, network_name: str, monitoring_interval: int = 5):
-    global stop_threads
+    logger.debug("Start 'monitor_containers_task' task...")
 
     try:
-        while not stop_threads:
-            containers_data = docker_client.list_containers_on_network(
-                network_name)
+        containers_data = docker_client.list_containers_on_network(
+            network_name)
 
-            logger.info("Containers on network '%s': %d",
-                        network_name, len(containers_data))
+        logger.info("Containers on network '%s': %d",
+                    network_name, len(containers_data))
 
-            containers_data = json.dumps([data.to_dict() for data in containers_data])
+        containers_data = json.dumps([data.to_dict() for data in containers_data])
 
-            producer.produce('containers-data', key='my_key',
-                             value=containers_data)
+        producer.produce('containers-data', key='my_key',
+                         value=containers_data)
 
-            time.sleep(monitoring_interval)
-
-    except KeyboardInterrupt:
-        logger.info("Stopping thread 'monitor_containers_task'...")
-        pass
+        time.sleep(monitoring_interval)
 
     except Exception as e:
         logger.error("Error monitoring containers: %s", e)
-        pass
-
-    logger.info("Stopping thread 'monitor_containers_task'...")
 
 
 def create_container_task(consumer: Consumer, containers_data_producer: Producer,
                           base_image_path: str, network_name: str):
-    global stop_threads
+    logger.debug("Start 'create_container_task' task...")
 
-    while not stop_threads:
-        try:
-            message = consume_kafka_message(consumer)
-            if message is None:
-                continue
+    try:
+        message = consume_kafka_message(consumer)
+        if message is None:
+            return
 
-            logger.info("Creating container with message: %s", message)
-            docker_client.create_docker_container(base_image_path=base_image_path, network_name=network_name,
-                                                  create_request=message)
+        logger.info("Creating container with message: %s", message)
+        docker_client.create_docker_container(base_image_path=base_image_path, network_name=network_name,
+                                              create_request=message)
 
-            logger.info("Container created successfully")
-            feedback: FeedbackMessage = FeedbackMessage("Container created successfully", Level.SUCCESS)
-            containers_data_producer.produce('containers-data-feedback', key='my_key', value=feedback.to_json())
+        logger.info("Container created successfully")
+        feedback: FeedbackMessage = FeedbackMessage("Container created successfully", Level.SUCCESS)
+        containers_data_producer.produce('containers-data-feedback', key='my_key', value=feedback.to_json())
 
-        except KeyboardInterrupt:
-            logger.info("Stopping thread 'create_container_task'...")
-            break
-
-        except Exception as e:
-            logger.error("Error creating container: %s", e)
-            feedback: FeedbackMessage = FeedbackMessage(str(e), Level.ERROR)
-            containers_data_producer.produce('containers-data-feedback', key='my_key', value=feedback.to_json())
-
-    logger.info("Stopping thread 'create_container_task'...")
+    except Exception as e:
+        logger.error("Error creating container: %s", e)
+        feedback: FeedbackMessage = FeedbackMessage(str(e), Level.ERROR)
+        containers_data_producer.produce('containers-data-feedback', key='my_key', value=feedback.to_json())
 
 
 def delete_container_task(consumer: Consumer, containers_data_producer: Producer):
-    global stop_threads
+    logger.debug("Start 'delete_container_task' task...")
 
     try:
-        while not stop_threads:
-            message = consume_kafka_message(consumer)
-            if message is None:
-                continue
+        message = consume_kafka_message(consumer)
+        if message is None:
+            return
 
-            logger.info("Deleting container with id: %s", message)
-            docker_client.delete_docker_container(container_id=message)
+        logger.info("Deleting container with id: %s", message)
+        docker_client.delete_docker_container(container_id=message)
 
-            logger.info("Container deleted successfully")
-            feedback: FeedbackMessage = FeedbackMessage("Container deleted successfully", Level.SUCCESS)
-            containers_data_producer.produce('containers-data-feedback', key='my_key', value=feedback.to_json())
-
-    except KeyboardInterrupt:
-        logger.info("Stopping thread 'delete_container_task'...")
-        pass
+        logger.info("Container deleted successfully")
+        feedback: FeedbackMessage = FeedbackMessage("Container deleted successfully", Level.SUCCESS)
+        containers_data_producer.produce('containers-data-feedback', key='my_key', value=feedback.to_json())
 
     except Exception as e:
         logger.error("Error deleting container: %s", e)
         pass
 
-    logger.info("Stopping thread 'delete_container_task'...")
-
-
-def build_delete_container_task(delete_container_consumer: Consumer,
-                                containers_data_producer: Producer) -> threading.Thread:
-    return threading.Thread(target=delete_container_task, args=(delete_container_consumer, containers_data_producer))
-
-
-def build_create_container_task(base_image_path, create_container_consumer, containers_data_producer,
-                                network_name) -> threading.Thread:
-    return threading.Thread(target=create_container_task,
-                            args=(create_container_consumer, containers_data_producer, base_image_path, network_name))
-
-
-def build_monitor_containers_task(containers_data_producer, monitoring_interval, network_name) -> threading.Thread:
-    return threading.Thread(target=monitor_containers_task,
-                            args=(containers_data_producer, network_name, monitoring_interval))
-
 
 def main():
-    global stop_threads
-
     # Load the configuration
     config = config_loader.load_config(os.path.abspath(__file__))
     kafka_url = config.get('kafka', 'bootstrap_servers')
@@ -178,51 +125,31 @@ def main():
     delete_container_consumer = create_kafka_consumer('delete-container', 'my-group-delete-container',
                                                       kafka_url)
 
-    # Create threads
-    threads = {
-        'monitor_containers': build_monitor_containers_task(containers_data_producer, monitoring_interval,
-                                                            network_name),
-        'create_container': build_create_container_task(base_image_path, create_container_consumer,
-                                                        containers_data_producer,
-                                                        network_name),
-        'delete_container': build_delete_container_task(delete_container_consumer, containers_data_producer)
-    }
+    # Create thread pool
+    thread_pool = ThreadPool(monitor_interval=monitoring_interval)
+
+    # Add tasks to thread pool
+    thread_pool.add_task(name='monitor_containers', target=monitor_containers_task,
+                         args=(containers_data_producer, network_name, monitoring_interval))
+
+    thread_pool.add_task(name='create_container', target=create_container_task,
+                         args=(create_container_consumer, containers_data_producer, base_image_path, network_name))
+
+    thread_pool.add_task(name='delete_container', target=delete_container_task,
+                         args=(delete_container_consumer, containers_data_producer))
 
     # Set up signal handler for Ctrl+C
-    signal.signal(signal.SIGINT, stop_threads_handler(threads, containers_data_producer,
-                                                      create_container_consumer,
-                                                      delete_container_consumer))
+    signal.signal(signal.SIGINT, stop_threads_handler(
+        thread_pool=thread_pool,
+        containers_data_producer=containers_data_producer,
+        create_container_consumer=create_container_consumer,
+        delete_container_consumer=delete_container_consumer))
 
     # Start the threads
-    for thread_name, thread in threads.items():
-        logger.info("Starting thread '%s'...", thread_name)
-        thread.start()
+    thread_pool.start_threads()
 
-    try:
-        while not stop_threads:
-            for thread_name, thread in threads.items():
-                if not thread.is_alive():
-                    logger.error("Thread '%s' is not alive. Restarting...", thread_name)
-
-                    if thread_name == 'monitor_containers':
-                        threads[thread_name] = build_monitor_containers_task(containers_data_producer,
-                                                                             monitoring_interval,
-                                                                             network_name)
-
-                    elif thread_name == 'create_container':
-                        threads[thread_name] = (base_image_path, create_container_consumer,
-                                                network_name)
-
-                    elif thread_name == 'delete_container':
-                        threads[thread_name] = build_delete_container_task(
-                            delete_container_consumer, containers_data_producer)
-
-                    threads[thread_name].start()
-
-            time.sleep(monitoring_interval)
-    finally:
-        stop_application(threads, containers_data_producer, create_container_consumer, delete_container_consumer)
-        logger.info("All threads stopped. Exiting...")
+    # Monitor threads
+    thread_pool.monitor_threads()
 
     logger.info("Exiting...")
     sys.exit(0)
--- monitoring/threads/thread_pool.py
+++ monitoring/threads/thread_pool.py
@@ -1,10 +1,16 @@
+import logging
 import threading
+import time
+
+# Configure the logger
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 class AtomicBoolean:
-    def __init__(self):
+    def __init__(self, value):
         self._lock = threading.Lock()
-        self._value = False
+        self._value = value
 
     def get(self):
         with self._lock:
@@ -14,7 +20,8 @@ class AtomicBoolean:
         with self._lock:
             self._value = value
 
-class ThreadTask:
+
+class Task:
     def __init__(self, name, target, args):
         self._name = name
         self._target = target
@@ -29,20 +36,51 @@ class ThreadTask:
     def get_args(self):
         return self._args
 
+    def task_wrapper(self, is_running: AtomicBoolean):
+        while is_running.get():
+            self._target(*self._args)
+
+        logger.info("Thread '%s' stopped", self._name)
+
+    def build_thread(self, is_running: AtomicBoolean):
+        return threading.Thread(target=self.task_wrapper, args=(is_running,))
+
+
 class ThreadPool:
-    def __init__(self):
-        self._threads = {}
+    def __init__(self, monitor_interval):
+        self._threads: dict[str, threading.Thread] = {}
+        self._tasks: dict[str, Task] = {}
+        self._monitor_interval = monitor_interval
+        self._threads_running = AtomicBoolean(True)
 
-    def add_thread(self, name, thread):
-        self._threads[name] = thread
+    def add_task(self, name, target, args):
+        self._tasks[name] = Task(name, target, args)
 
-    def get_thread(self, name):
-        return self._threads[name]
-
-    def get_threads(self):
-        return self._threads
+    def start_threads(self):
+        for task_name, task in self._tasks.items():
+            self._threads[task_name] = task.build_thread(self._threads_running)
+            self._threads[task_name].start()
 
     def stop_threads(self):
-        for name, thread in self._threads.items():
+        self._threads_running.set(False)
+
+        for thread_name, thread in self._threads.items():
+            logger.info("Stopping thread '%s'...", thread_name)
             thread.join()
-            print(f"Thread '{name}' stopped.")
+
+        logger.info("All threads stopped")
+
+    def monitor_threads(self):
+        try:
+            while self._threads_running.get():
+                for thread_name, thread in self._threads.items():
+                    if not thread.is_alive():
+                        logger.info("Thread '%s' is not alive. Restarting...", thread_name)
+
+                        self._threads[thread_name] = self._tasks[thread_name].build_thread(self._threads_running)
+                        self._threads[thread_name].start()
+
+                time.sleep(self._monitor_interval)
+        except KeyboardInterrupt:
+            logger.info("Interrupt signal received. Stopping application...")
+            self.stop_threads()
