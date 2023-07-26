5bf5d3b1dfaedbdc43c9cf2a305f5e2c0fa828dc
vladtf <vlad.tiftilov@yahoo.com>
2023-07-26 17:54:15 +0300

    refactor jar upload logic

--- backend/src/main/java/vti/containerization/backend/upload/UploadArtifactService.java
+++ backend/src/main/java/vti/containerization/backend/upload/UploadArtifactService.java
@@ -1,25 +1,24 @@
 package vti.containerization.backend.upload;
 
-import aj.org.objectweb.asm.ClassReader;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import vti.containerization.backend.configuration.ContainerizationToolProperties;
-import vti.containerization.backend.upload.jar.JarInfoResponse;
-import vti.containerization.backend.upload.jar.MainClassVisitor;
+import vti.containerization.backend.upload.jar.JarUploadService;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.jar.*;
+import java.util.jar.JarInputStream;
+import java.util.jar.Manifest;
 
 import static vti.containerization.backend.utils.FileUtils.generateUniqueFileName;
 
@@ -29,6 +28,7 @@ import static vti.containerization.backend.utils.FileUtils.generateUniqueFileNam
 public class UploadArtifactService {
 
     private final ContainerizationToolProperties containerizationToolProperties;
+    private final JarUploadService jarUploadService;
 
 
     @SneakyThrows
@@ -90,7 +90,7 @@ public class UploadArtifactService {
             if (uploadedFileModel.getType() == UploadedFileModel.FileType.JAR) {
                 try (JarInputStream jarInputStream = new JarInputStream(new FileInputStream(file))) {
                     Manifest manifest = jarInputStream.getManifest();
-                    String mainClassNameFromManifest = getMainClassNameFromManifest(manifest);
+                    String mainClassNameFromManifest = jarUploadService.getMainClassNameFromManifest(manifest);
                     uploadedFileModel.setJavaMainClass(mainClassNameFromManifest);
                 }
             }
@@ -132,108 +132,10 @@ public class UploadArtifactService {
         }
     }
 
-
-    public JarInfoResponse getJarInfo(MultipartFile jar) {
-        try (JarInputStream jarInputStream = new JarInputStream(jar.getInputStream())) {
-            JarInfoResponse response = new JarInfoResponse();
-
-            // Check if the JAR contains a Manifest file
-            Manifest manifest = jarInputStream.getManifest();
-
-            String mainClassNameFromManifest = getMainClassNameFromManifest(manifest);
-            response.setMainClassName(mainClassNameFromManifest);
-
-            response.setClassesWithMainMethod(scanForClassesWithMain(jarInputStream));
-            return response;
-        } catch (IOException e) {
-            // Handle any potential IOException here
-            e.printStackTrace();
-            // You may also set an error flag in the response or throw a custom exception
-        }
-
-        throw new RuntimeException("Failed to get JAR info");
-    }
-
-    private String getMainClassNameFromManifest(Manifest manifest) throws IOException {
-        if (manifest == null) {
-            return null;
-        }
-
-        Attributes attributes = manifest.getMainAttributes();
-        return attributes.getValue("Main-Class");
-    }
-
-    private List<String> scanForClassesWithMain(JarInputStream jarInputStream) throws IOException {
-        List<String> classesWithMain = new ArrayList<>();
-
-        JarEntry entry;
-        while ((entry = jarInputStream.getNextJarEntry()) != null) {
-            if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
-                ClassReader classReader = new ClassReader(jarInputStream);
-                MainClassVisitor visitor = new MainClassVisitor();
-                classReader.accept(visitor, ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
-                if (visitor.hasMainMethod()) {
-                    classesWithMain.add(visitor.getClassName());
-                }
-            }
-        }
-
-        return classesWithMain;
-    }
-
     @SneakyThrows
     public void handleJarUpload(MultipartFile file, String selectedMainClass) {
-        JarInfoResponse jarInfo = getJarInfo(file);
-
-        if (jarInfo.getMainClassName() != null && jarInfo.getMainClassName().equals(selectedMainClass)) {
-            handleArtifactUpload(file);
-            return;
-        }
-
-        if (!jarInfo.getClassesWithMainMethod().contains(selectedMainClass)) {
-            throw new RuntimeException("The selected main class doesn't contain a main method");
-        }
-
-        // Save the uploaded file to a temporary location
-        Path tempDir = Files.createTempDirectory("uploaded-jar");
-        Path jarPath = tempDir.resolve(file.getOriginalFilename());
-        Files.copy(file.getInputStream(), jarPath, StandardCopyOption.REPLACE_EXISTING);
-
-        try (JarFile jar = new JarFile(jarPath.toFile())) {
-            // Load the JAR file and get the manifest
-            Manifest manifest = jar.getManifest();
-
-            // Update the Main-Class attribute in the manifest
-            manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS, selectedMainClass);
-
-            // Create an in-memory buffer to store the updated JAR
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            try (JarOutputStream jos = new JarOutputStream(baos, manifest)) {
-                for (JarEntry entry : Collections.list(jar.entries())) {
-                    if (!entry.getName().equalsIgnoreCase("META-INF/MANIFEST.MF")) {
-                        try (InputStream is = jar.getInputStream(entry)) {
-                            jos.putNextEntry(new JarEntry(entry.getName()));
-                            byte[] buffer = new byte[1024];
-                            int bytesRead;
-                            while ((bytesRead = is.read(buffer)) != -1) {
-                                jos.write(buffer, 0, bytesRead);
-                            }
-                            jos.flush();
-                            jos.closeEntry();
-                        }
-                    }
-                }
-            }
-
-            // Save the updated JAR to disk
-            Files.write(jarPath, baos.toByteArray());
-
-            // Now handle the updated JAR file
-            File updatedJarFile = jarPath.toFile();
-            handleArtifactUpload(updatedJarFile);
-        }
-
-        // Clean up the temporary file
-        Files.delete(jarPath);
+        File jarUpload = jarUploadService.handleJarUpload(file, selectedMainClass);
+        handleArtifactUpload(jarUpload);
+        Files.deleteIfExists(jarUpload.toPath());
     }
 }
--- backend/src/main/java/vti/containerization/backend/upload/jar/JarUploadService.java
+++ backend/src/main/java/vti/containerization/backend/upload/jar/JarUploadService.java
@@ -1,9 +1,135 @@
 package vti.containerization.backend.upload.jar;
 
 
+import aj.org.objectweb.asm.ClassReader;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.jar.*;
 
 @Service
 public class JarUploadService {
+
+    public JarInfoResponse getJarInfo(MultipartFile jar) {
+        try (JarInputStream jarInputStream = new JarInputStream(jar.getInputStream())) {
+            JarInfoResponse response = new JarInfoResponse();
+
+            // Check if the JAR contains a Manifest file
+            Manifest manifest = jarInputStream.getManifest();
+
+            String mainClassNameFromManifest = getMainClassNameFromManifest(manifest);
+            response.setMainClassName(mainClassNameFromManifest);
+
+            response.setClassesWithMainMethod(scanForClassesWithMain(jarInputStream));
+            return response;
+        } catch (IOException e) {
+            // Handle any potential IOException here
+            e.printStackTrace();
+            // You may also set an error flag in the response or throw a custom exception
+        }
+
+        throw new RuntimeException("Failed to get JAR info");
+    }
+
+    public String getMainClassNameFromManifest(Manifest manifest) throws IOException {
+        if (manifest == null) {
+            return null;
+        }
+
+        Attributes attributes = manifest.getMainAttributes();
+        return attributes.getValue("Main-Class");
+    }
+
+    private List<String> scanForClassesWithMain(JarInputStream jarInputStream) throws IOException {
+        List<String> classesWithMain = new ArrayList<>();
+
+        JarEntry entry;
+        while ((entry = jarInputStream.getNextJarEntry()) != null) {
+            if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
+                ClassReader classReader = new ClassReader(jarInputStream);
+                MainClassVisitor visitor = new MainClassVisitor();
+                classReader.accept(visitor, ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
+                if (visitor.hasMainMethod()) {
+                    classesWithMain.add(visitor.getClassName());
+                }
+            }
+        }
+
+        return classesWithMain;
+    }
+
+    @SneakyThrows
+    public File handleJarUpload(MultipartFile file, String selectedMainClass) {
+        JarInfoResponse jarInfo = getJarInfo(file);
+
+        if (jarInfo.getMainClassName() != null && jarInfo.getMainClassName().equals(selectedMainClass)) {
+            // No need to update the JAR, return the original file directly
+            return fileToTempFile(file);
+        }
+
+        if (!jarInfo.getClassesWithMainMethod().contains(selectedMainClass)) {
+            throw new RuntimeException("The selected main class doesn't contain a main method");
+        }
+
+        // Save the uploaded file to a temporary location
+        Path tempDir = Files.createTempDirectory("uploaded-jar");
+        Path jarPath = tempDir.resolve(file.getOriginalFilename());
+        Files.copy(file.getInputStream(), jarPath, StandardCopyOption.REPLACE_EXISTING);
+
+        try (JarFile jar = new JarFile(jarPath.toFile())) {
+            // Load the JAR file and get the manifest
+            Manifest manifest = jar.getManifest();
+
+            // Update the Main-Class attribute in the manifest
+            manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS, selectedMainClass);
+
+            // Create an in-memory buffer to store the updated JAR
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            try (JarOutputStream jos = new JarOutputStream(baos, manifest)) {
+                for (JarEntry entry : Collections.list(jar.entries())) {
+                    if (!entry.getName().equalsIgnoreCase("META-INF/MANIFEST.MF")) {
+                        try (InputStream is = jar.getInputStream(entry)) {
+                            jos.putNextEntry(new JarEntry(entry.getName()));
+                            byte[] buffer = new byte[1024];
+                            int bytesRead;
+                            while ((bytesRead = is.read(buffer)) != -1) {
+                                jos.write(buffer, 0, bytesRead);
+                            }
+                            jos.flush();
+                            jos.closeEntry();
+                        }
+                    }
+                }
+            }
+
+            // Save the updated JAR to disk
+            Files.write(jarPath, baos.toByteArray());
+
+            // Return the updated JAR file
+            return jarPath.toFile();
+        }
+    }
+
+    // Helper method to convert MultipartFile to temporary File
+    private File fileToTempFile(MultipartFile file) throws IOException {
+        File tempFile = File.createTempFile("uploaded-jar", file.getOriginalFilename());
+        try (InputStream inputStream = file.getInputStream();
+             OutputStream outputStream = new FileOutputStream(tempFile)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        }
+        return tempFile;
+    }
 
 }
